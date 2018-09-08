2409204999a5ae36f0144ab6a7e8ed6f9c8e2aa4
abhinav <abhinavashok@live.in>
2018-09-08 22:13:28 +0530

    added union and isConnected method

--- Algorithm/QuickFind.java
+++ Algorithm/QuickFind.java
@@ -22,4 +22,48 @@ class QuickFind{
     Write a program to perform union and isconnected operations as can be seen
     in the above explanation
      */
+
+    private int[] id;
+    
+    public QuickFind(int size){
+        id = new int[size];
+        initialize();
+    }
+
+    private void initialize(){
+        for(int i=0;i<id.length;i++){
+            id[i] = i;
+        }
+    }
+
+    public void union(int node1,int node2){
+        int previous = id[node1];
+        int current = id[node2];
+
+        if(previous == current){
+            return;
+        }
+
+        for(int i=0;i<id.length;i++){
+            if(id[i] == previous){
+                id[i] = node2;
+            }
+        }
+
+
+    }
+
+    public boolean isConnected(int node1,int node2){
+        return (id[node1] == id[node2]);
+    }
+    
+    public static void main(String[] args) {
+        QuickFind inst = new QuickFind(8);
+        inst.union(4,7);
+        inst.union(5,7);
+        inst.printIds();
+        inst.union(7,0);
+        inst.printIds();
+        System.out.println(inst.isConnected(0,7));
+    }
 }
