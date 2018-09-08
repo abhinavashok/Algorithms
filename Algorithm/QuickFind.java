9470b39657be3a11ae599ea1186257f6942da222
Abhinav <abhinavashok@live.in>
2018-09-08 17:20:16 +0530

    adding problem statement for quickfind

---
+++ Algorithm/QuickFind.java
@@ -0,0 +1,25 @@
+class QuickFind{
+    /**
+    Two operations:
+    1.union
+    2.isConnected
+
+    Index:		 1,2,3,4,5,6,7,8
+
+    Value in array : 1,2,3,4,5,6,7,8
+
+    Union(4,8),Union(3,7),Union(7,8),Union(1,2)
+
+    Points state after above operation:
+        1   2	3	4
+		        |	|
+        5	6	7___8
+
+        
+    isConnected(3,8) : True
+    isConnected(1,8) : False
+    
+    Write a program to perform union and isconnected operations as can be seen
+    in the above explanation
+     */
+}
